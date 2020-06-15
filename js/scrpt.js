ca98cb2ad4cef5bd9eac5a722dfe1ffb0620c457
IvanVeskovic <weeskaa@gmail.com>
2020-06-15 22:08:53 +0200

    half carousel done

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,4 +1,54 @@
 const inputs = document.querySelectorAll('input');
+const carousels= document.querySelectorAll('.carousel__img');
+const carouselBtns = document.querySelectorAll('.carousel__box');
+const carouselTracker = document.querySelector('.carousel__tracker');
+
+
+const carouselImages = ['../img/carousel-1.jpg', '../img/carousel-2.jpg' , '../img/carousel-3.jpg'];
+
+
+//Remove width from carousel img
+function removeWidth () {
+    carousels.forEach(el => {
+        el.style.width = '0';
+    })
+} 
+
+// Change carousel on click
+carouselTracker.addEventListener('click', function(e) {
+    if(e.target.classList.contains('carousel__box')){
+        removeWidth();
+        
+        let element = +(e.target.classList[1].slice(-1)) - 1;
+        carousels[element].style.width = "100%";
+    }
+    e.preventDefault();
+})
+
+
+// function start() {
+
+//     setTimeout(function() {
+//         console.log('Hello My Infinite Loop Execution');
+
+//       // Again
+//       start();
+
+//       // Every 3 sec
+//     }, 3000);
+// }
+
+// // Begins
+// start();
+
+
+autoScrollCarousel();
+
+
+
+
+
+
 
 
 
